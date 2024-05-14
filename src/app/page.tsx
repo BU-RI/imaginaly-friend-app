a69c8e4c6514702c033d273bc0368d9b9922f8bd
BU-RI <proaca779@gmail.com>
2024-05-14 16:13:04 +0900

    いったんプッシュ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+'use client'
+import Textfield from '@mui/material/TextField'
+import Box from '@mui/material/Box'
+import { useState } from 'react'
 
 export default function Home() {
+  const [ inputvalue, setInputvalue ] = useState("")
   return (
       <div>
-        <p>imaginary</p>
+        <h1 className="title">imaginary-friend-app</h1>
+        <div className="boxone">
+        <Box component="section" sx={{ p: 2, border: '1px dashed black',width: '300px',   flexDirection:'row', display:'flex' }}><Textfield value={inputvalue} sx={{ fontsize: "5px" ,width: "300px" , }}onChange={(e) => setInputvalue(e.target.value)}  placeholder={inputvalue ? "" : "ここにしゃべりたいことを書いてね"} variant="standard" InputProps={{disableUnderline: true}}></Textfield></Box>
+          <Box component="section" sx={{ p: 2, border: '1px dashed black',width: '300px',   flexDirection:'row', display:'flex', paddingTop: "5px" ,}}><p>友達の返答欄</p></Box>
+        </div>
       </div>
   );
 }
